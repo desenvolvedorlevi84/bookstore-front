7cda031bfe41b7fb45704218d77928fb4cf496e4
desenvolvedorlevi84 <isaquelevi@gmail.com>
2023-03-01 10:55:43 -0300

    UPDATE para livro implementado

--- src/app/components/views/livro/livro-update/livro-update.component.ts
+++ src/app/components/views/livro/livro-update/livro-update.component.ts
@@ -7,11 +7,11 @@ import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-livro-create',
-  templateUrl: './livro-create.component.html',
-  styleUrls: ['./livro-create.component.css']
+  selector: 'app-livro-update',
+  templateUrl: './livro-update.component.html',
+  styleUrls: ['./livro-update.component.css']
 })
-export class LivroCreateComponent implements OnInit {
+export class LivroUpdateComponent implements OnInit {
 
   id_cat: String = ''
 
@@ -33,22 +33,29 @@ texto = new FormControl("", [Validators.minLength(10)]);
   ) { }
 
   ngOnInit(): void {
-    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!
-  }
-
-  create(): void {
-    this.service.create(this.livro, this.id_cat).subscribe((resposta) => {
-      this.router.navigate([`categorias/${this.id_cat}/livros`]);
-      this.service.mensagem("Livro criado com sucesso!");
-    }, err => {
-      this.router.navigate([`categorias/${this.id_cat}/livros`]);
-      this.service.mensagem("Erro ao criar novo livro! Tente mais tarde!");
-
-    });
+    this.id_cat = this.route.snapshot.paramMap.get("id_cat")!
+    this.livro.id = this.route.snapshot.paramMap.get("id")!
+    this.findById()
   }
 
   cancel(): void {
     this.router.navigate([`categorias/${this.id_cat}/livros`]);
+  }
+
+  findById(): void {
+    this.service.findById(this.livro.id!).subscribe((resposta) =>{
+      this.livro = resposta
+    })
+  }
+
+  update(): void {
+    this.service.update(this.livro).subscribe((resposta) => {
+      this.router.navigate([`categorias/${this.id_cat}/livros`]);
+      this.service.mensagem('Livro atualizado com sucesso!')
+    }, err => {
+      this.router.navigate([`categorias/${this.id_cat}/livros`]);
+      this.service.mensagem('Falha ao atualizar livro! Tente mais tarde..')
+    })
   }
 
   getMessage () {
