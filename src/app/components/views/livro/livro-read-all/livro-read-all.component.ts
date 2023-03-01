73169fbd9664676b5367daa62edc58dc72382fd8
desenvolvedorlevi84 <isaquelevi@gmail.com>
2023-02-28 21:53:26 -0300

    funcao adicionar novo livro implementada com sucesso

--- src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
+++ src/app/components/views/livro/livro-read-all/livro-read-all.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
@@ -17,7 +18,7 @@ export class LivroReadAllComponent implements OnInit {
 
   livros: Livro[] = [];
 
-  constructor(private service: LivroService, private route: ActivatedRoute) { }
+  constructor(private service: LivroService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
     this.id_cat = this.route.snapshot.paramMap.get('id_cat')!;
@@ -29,5 +30,9 @@ export class LivroReadAllComponent implements OnInit {
       this.livros = resposta;
       console.log(this.livros)
     })
+  }
+
+  navegarParaCriarLivro(): void {
+    this.router.navigate([`categorias/${this.id_cat}/livros/create`])
   }
 }
